d9054d6f66e20017d93e23be1b31890f037090d8
kdjay517 <kdjay517@gmail.com>
2021-10-08 22:27:06 +0530

    added result3 condition in uc4

---
+++ soritngArthcomputation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+read -p "Enter the first Number:" a
+read -p "Enter the second Number:" b
+read -p "Enter the third Number:" c
+
+result_1=$(($a+$b+$c))
+result_2=$(($a*$b*$c))
+result_3=$(echo "scale=2; $c+$a/$b"|bc)
+
