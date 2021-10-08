259c7bb6f36ea166a61063cc244616a03bc94072
kdjay517 <kdjay517@gmail.com>
2021-10-08 23:47:22 +0530

    added dictionary elements to array

---
+++ soritngArthcomputation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+read -p "Enter the first Number:" a
+read -p "Enter the second Number:" b
+read -p "Enter the third Number:" c
+
+
+declare -A dictionary_of_results;
+
+result_1=$(($a+$b+$c))
+result_2=$(($a*$b*$c))
+result_3=$(echo "scale=2; $c+$a/$b"|bc)
+result_4=$(echo "scale=2; $a%$b+$c"|bc)
+
+
+dictionary_of_results[0]=$result_1;
+dictionary_of_results[1]=$result_2;
+dictionary_of_results[2]=$result_3;
+dictionary_of_results[3]=$result_4;
+
+for ((i=0; i<=3; i++))
+do
+	Array[i]=${dictionary_of_results[$i]}
+done
